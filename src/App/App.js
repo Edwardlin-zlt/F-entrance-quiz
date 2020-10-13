744969d8811b83776ad5f722f2d8e06a62412641
lintao <lintao.zhang@thoughtworks.com>
2020-10-13 13:57:06 +0800

    [Lintao] feat: new GroupList page

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import GroupList from '../Pages/GroupList/GroupList';
 
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+        <GroupList />
       </div>
     );
   }
---
+++ src/Pages/GroupList/GroupList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class GroupList extends Component {
+  render() {
+    return (
+      <div>
+        <div className="list-title">
+          <h3>分组列表 </h3>
+          <button type="button">分组学员</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GroupList;
