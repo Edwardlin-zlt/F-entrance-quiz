1a690e6cc7e7735e6300c961d0999b4e27b4bb44
lintao <lintao.zhang@thoughtworks.com>
2020-10-13 14:37:07 +0800

    [Lintao] feat: new all trainees section

--- src/Pages/GroupList/GroupList.js
+++ src/Pages/GroupList/GroupList.js
@@ -5,22 +5,32 @@ class GroupList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // TODO trainee: [],
+      trainees: ['lintao', 'hahah'],
       groups: [['asd', 'asdf'], ['asd'], ['asd'], ['asd'], ['asd'], []],
     };
   }
 
   render() {
-    const { groups } = this.state;
+    const { groups, trainees } = this.state;
     return (
-      <div>
-        <div className="list-title">
+      <div className="groups">
+        <section className="groups-header">
           <h3>分组列表</h3>
           <button type="button">分组学员</button>
-        </div>
-        {groups.map((groupTrainee, index) => (
-          <TraineeGroupRow groupTrainee={groupTrainee} groupIndex={index} key={index} />
-        ))}
+        </section>
+        <section className="groups-main">
+          {groups.map((groupTrainee, index) => (
+            <TraineeGroupRow groupTrainee={groupTrainee} groupIndex={index} key={index} />
+          ))}
+        </section>
+        <section className="groups-all-trainee">
+          <h3>学员列表</h3>
+          {trainees.map((traineeName, index) => (
+            <span className="trainee-name" key={index}>
+              {index + 1}.{traineeName}
+            </span>
+          ))}
+        </section>
       </div>
     );
   }
