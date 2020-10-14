f64be40ab36d0389dfa02b77d0925e7c1de47760
杜娟 <jdu.xian@gmail.com>
2020-10-14 15:20:55 +0800

    feedback from Du Juan

--- src/Api/trainee.test.js
+++ src/Api/trainee.test.js
@@ -10,6 +10,7 @@ describe('TraineeApi', () => {
       axios.get.mockResolvedValue({ trainees: {} });
       await TraineeApi.getAll();
 
+      // TODO feedback: 这两个测试都是测试了TraineeApi.getAll的调用，所以可以合并
       expect(axios.get).toHaveBeenCalledWith('base.url.com/trainees');
     });
 
--- src/Pages/GroupList/GroupList.js
+++ src/Pages/GroupList/GroupList.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import TraineeGroupRow from '../../Components/GroupTrainee/TraineeGroupRow';
-import './GroupList.css';
+// TODO feedback: 代码运行失败，因为没有css这个文件
 import TraineeApi from '../../Api/trainee';
 
+// TODO feedback:组件划分不够合理，划分层次不够，导致所有的代码都在这个组件中，很难维护
 class GroupList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       trainees: [],
+      // TODO feedback: 不建议定义6个数组去维护数据，每次操作会非常麻烦，建议直接去操作后台反馈的groups列表
       groups: [[], [], [], [], [], []],
     };
   }
 
   async componentDidMount() {
     await this.getAllTrainees();
+    // TODO feedback: 和需求不符，初始情况并不会分组
     this.groupTrainees();
   }
 
   getAllTrainees = async () => {
     const trainees = await TraineeApi.getAll();
+    // TODO feedback: 后台直接反馈了name数据，不需要再map一次了
     const traineeNames = trainees.map((traineeObj) => traineeObj.name);
     this.setState({ trainees: traineeNames });
   };
@@ -45,7 +49,9 @@ class GroupList extends Component {
   groupTrainees = () => {
     const { trainees } = this.state;
     const groups = [[], [], [], [], [], []];
+    // TODO feedback: trainees为数组，这种判断其实不起作用
     if (trainees) {
+      // TODO feedback: 分组是在后台实现
       const shuffledTrainees = this.shuffle([...trainees]);
       shuffledTrainees.forEach((trainee, index) => {
         groups[index % 6].push(trainee);
@@ -58,6 +64,7 @@ class GroupList extends Component {
     const { groups, trainees } = this.state;
     return (
       <div className="groups">
+        {/* // TODO feedback: 既然是groups-header，为什么不用header标签 */}
         <section className="groups-header">
           <h3 className="groups-header-title">分组列表</h3>
           <button type="button" onClick={this.groupTrainees}>
@@ -66,10 +73,12 @@ class GroupList extends Component {
         </section>
         <section className="groups-main">
           {groups.map((groupTrainee, index) => (
+            // TODO feedback: 需求是希望后台直接返回组名，而不是前端通过index维护
             <TraineeGroupRow groupTrainee={groupTrainee} groupIndex={index} key={index} />
           ))}
         </section>
         <section className="groups-all-trainee">
+          {/* // TODO feedback: 学员列表不应该包含在GroupList组件中，相互之间没有业务包含关系 */}
           <h3>学员列表</h3>
           {trainees &&
             trainees.map((traineeName, index) => (
